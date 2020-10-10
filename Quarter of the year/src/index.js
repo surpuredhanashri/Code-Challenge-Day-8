a752978fe73e775472a2c65c010bf2975003ec55
surpuredhanashri <surpuredhanashri@gmail.com>
2020-10-10 20:50:45 +0530

    done

--- Quarter of the year/src/index.js
+++ Quarter of the year/src/index.js
@@ -6,7 +6,14 @@
 
 // Write the function to find the quarterOf obtained.
 const quarterOf = (month) => {
-    //your code here
-  }
+  if(month>=1&&month<=3)
+  return 1;
+  else if(month>=4&&month<=6)
+  return 2;
+  else if(month>=7&&month<=9)
+  return 3;
+  else
+  return 4;
+}
 
 //Use SpecRunner to check the Test Cases.
--- Tip Calculator/src/index.js
+++ Tip Calculator/src/index.js
@@ -12,7 +12,20 @@
 
 // Write the function to find the calculateTip obtained.
 function calculateTip(amount, rating) {
-    //your code here
+  switch(rating)
+  {
+    case 'Excellent':
+      return Math.round((amount*20)/100);
+    case 'great':
+      return Math.round((amount*15)/100);
+    case 'good':
+      return Math.round((amount*10)/100);
+    case 'Poor':
+      return Math.round((amount*5)/100);
+    case 'Terrible':
+      return (amount*0);
+    default:
+      return "Rating not recognised";
   }
-
+}
 //Use SpecRunner to check the Test Cases.
